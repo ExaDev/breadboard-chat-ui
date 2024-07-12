59377b91f0081089714c43ecea50856d2e409035
Joseph Mearman <joseph@mearman.co.uk>
2024-07-12 12:19:16 +0100

    chore: Update breadboardInvoker to use @google-labs/breadboard library

--- src/breadboard/breadboardInvoker.ts
+++ src/breadboard/breadboardInvoker.ts
@@ -1,4 +1,13 @@
-import { BreadboardUrl, LlmContext } from "./types";
+import {
+	BoardRunner,
+	Kit,
+	OutputValues,
+	asRuntimeKit
+} from "@google-labs/breadboard";
+import { RunConfig, run } from "@google-labs/breadboard/harness";
+import { InputResolveRequest } from "@google-labs/breadboard/remote";
+import Core from "@google-labs/core-kit";
+import { BreadboardUrl, LlmContext, isLlmContext } from "./types";
 
 export type BreadboardInvokerCallback = (contextData: LlmContext) => void;
 
@@ -11,17 +20,45 @@ export const invokeBreadboard = async ({
 	boardURL: BreadboardUrl;
 	callback: BreadboardInvokerCallback;
 }) => {
-	await sleep(1000);
-	callback([
-		{
-			role: "model",
-			parts: [
-				{
-					text: Math.random() > 0.5 ? "cat" : "helloWorld",
+	const response = await fetch(boardURL);
+	const board = await response.json();
+
+	const runner: BoardRunner = await BoardRunner.fromGraphDescriptor(board);
+
+	const runTimeKits: Kit[] = [asRuntimeKit(Core)];
+
+	const runConfig: RunConfig = {
+		url: ".",
+		kits: runTimeKits,
+		remote: undefined,
+		proxy: undefined,
+		diagnostics: true,
+		runner: runner,
+	};
+
+	for await (const runResult of run(runConfig)) {
+		if (runResult.type === "input") {
+			await runResult.reply({
+				inputs: {
+					context,
 				},
-			],
-		},
-	]);
+			} satisfies InputResolveRequest);
+		} else if (runResult.type === "output") {
+			const resultOutputs: OutputValues = runResult.data.outputs;
+			console.log("output with Kit", JSON.stringify(resultOutputs, null, 2));
+			const context = resultOutputs.context;
+			if (!isLlmContext(context)) {
+				console.error(
+					"Invalid context",
+					resultOutputs.context
+						? JSON.stringify(resultOutputs.context, null, 2)
+						: "null"
+				);
+				continue;
+			}
+			await callback(context);
+		}
+	}
 };
 
 const sleep = async (ms: number) =>
