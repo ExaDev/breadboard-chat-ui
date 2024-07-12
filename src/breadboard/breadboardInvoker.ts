69a3b22fd088b0c39a4ad6cecda650161615bd0d
Joseph Mearman <joseph@mearman.co.uk>
2024-07-12 12:09:01 +0100

    chore: Refactor breadboardInvoker to use destructuring in invokeBreadboard function

--- src/breadboard/breadboardInvoker.ts
+++ src/breadboard/breadboardInvoker.ts
@@ -2,19 +2,27 @@ import { BreadboardUrl, LlmContext } from "./types";
 
 export type BreadboardInvokerCallback = (contextData: LlmContext) => void;
 
-export const invokeBreadboard = async (context: LlmContext, boardURL: BreadboardUrl, callback: BreadboardInvokerCallback) => {
+export const invokeBreadboard = async ({
+	context,
+	boardURL,
+	callback,
+}: {
+	context: LlmContext;
+	boardURL: BreadboardUrl;
+	callback: BreadboardInvokerCallback;
+}) => {
 	await sleep(1000);
 	callback([
 		{
 			role: "model",
 			parts: [
 				{
-					text: Math.random() > 0.5 ? "cat" : "helloWorld"
-				}
-			]
-		}
+					text: Math.random() > 0.5 ? "cat" : "helloWorld",
+				},
+			],
+		},
 	]);
-}
+};
 
 const sleep = async (ms: number) =>
 	new Promise((resolve) => setTimeout(resolve, ms));
