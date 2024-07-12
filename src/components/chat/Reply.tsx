470343dd8935b7b212f5e083932dd8e13432fe31
James Jackson <james.ogge@gmail.com>
2024-07-12 13:40:13 +0100

    add styles

--- src/components/chat/Reply.tsx
+++ src/components/chat/Reply.tsx
@@ -1,20 +1,33 @@
 import { PropsWithChildren } from "react";
 import layoutStyles from "../../styles/layout.module.scss";
 import { LlmRole } from "../../breadboard/types";
-import chatStyles from "./chat.module.scss"
+import chatStyles from "./chat.module.scss";
 import clsx from "clsx";
 
 type ReplyProps = { owner?: LlmRole } & PropsWithChildren;
 const Reply: React.FC<ReplyProps> = ({ children, owner }: ReplyProps) => {
 	return (
-		<div
-			className={clsx(
-				chatStyles.reply,
-				owner? chatStyles[owner] : chatStyles.model,
-				owner === LlmRole.model ? layoutStyles.alignLeft : layoutStyles.alignRight)
-			}
-		>
-			{children}
+		<div className={chatStyles.replyContainer}>
+			<h1
+				className={
+					owner === LlmRole.model
+						? layoutStyles.alignLeft
+						: layoutStyles.alignRight
+				}
+			>
+				{owner === LlmRole.model ? "Model" : "You"}
+			</h1>
+			<div
+				className={clsx(
+					chatStyles.reply,
+					owner ? chatStyles[owner] : chatStyles.model,
+					owner === LlmRole.model
+						? layoutStyles.alignLeft
+						: layoutStyles.alignRight
+				)}
+			>
+				{children}
+			</div>
 		</div>
 	);
 };
