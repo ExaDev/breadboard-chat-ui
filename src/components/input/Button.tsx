46bfbe2d78f61ad9f27a047077cd821eb299cbfa
James Jackson <james.ogge@gmail.com>
2024-07-19 10:11:48 +0100

    add alignment prop

--- src/components/input/Button.tsx
+++ src/components/input/Button.tsx
@@ -7,20 +7,25 @@ type ButtonProps = {
 	type?: "button" | "submit" | "reset";
 	title: string;
 	loading?: boolean;
+	align?: "flex-start" | "center" | "flex-end";
 };
 const Button: React.FC<ButtonProps> = ({
 	title,
 	onClick,
 	type = "button",
 	loading,
+	align = "flex-end"
 }: ButtonProps) => {
 	const handleClick = () => {
 		if (onClick) {
 			onClick();
 		}
 	};
+	const alignStyle = {
+		alignSelf: align
+	};
 	return (
-		<button type={type} onClick={handleClick} className={clsx(styles.button, loading ? styles.loading : undefined)}>
+		<button type={type} onClick={handleClick} style={alignStyle} className={clsx(styles.button, loading ? styles.loading : undefined)}>
 			{title}
 			{loading && <Spinner />}
 		</button>
