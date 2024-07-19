be5c09992771046252503b3ff0c7bf64c7d63958
James Jackson <james.ogge@gmail.com>
2024-07-19 15:24:24 +0100

    add basename

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 export const AppProvider: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<React.StrictMode>
-			<BrowserRouter>
+			<BrowserRouter basename="/breadboard-chat-ui/">
 				<BreadboardProvider>{children}</BreadboardProvider>
 			</BrowserRouter>
 		</React.StrictMode>
