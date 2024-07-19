d2c288109e977b46189e67da39e222fe0d7a0f25
James Jackson <james.ogge@gmail.com>
2024-07-19 10:10:23 +0100

    create canvas component

---
+++ src/components/chat/Canvas/Canvas.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+
+export type CanvasProps = {
+	width?: number;
+	height?: number;
+	onLoad?: (canvas2DContext: CanvasRenderingContext2D) => void;
+};
+const Canvas: React.FC<CanvasProps> = ({
+	width,
+	height,
+	onLoad,
+}) => {
+	const canvasRef = React.useRef<HTMLCanvasElement>(null);
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		if (canvas) {
+			const context = canvas.getContext("2d");
+			if (context && onLoad) {
+				onLoad(context);
+			}
+		}
+	}, [canvasRef, onLoad]);
+	return (
+		<canvas
+			width={width}
+			height={height}
+			ref={canvasRef}
+		/>
+	);
+};
+
+export default Canvas;
